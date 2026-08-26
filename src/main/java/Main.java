360e05b2de7219b2f881b41992f37ae0890efcc8
michaelinma <michael.inma@gmail.com>
2026-08-26 14:19:43 +0700

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hej världen123");
+        System.out.println("Hej världen, mitt namn är Michael. Dagens datum är 2026-08-26");
     }
 }
