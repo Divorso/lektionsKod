4b64b768f4fa3cdf72a231f822c4eb9e68120abd
michaelinma <michael.inma@gmail.com>
2026-08-26 16:24:06 +0700

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,7 +19,7 @@ public class Main {
         scanner.close();
 
          */
-
+        /*
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hej vad heter du");
         String name = scanner.nextLine();
@@ -27,5 +27,51 @@ public class Main {
         int age = scanner.nextInt();
 
         System.out.println("Hej " + name + ", du är: " + age + " gammal.");
+
+         */
+        /*
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Skriv tempraturen i celsius: ");
+        double celsius = scanner.nextDouble();
+
+        double fahrenheit = celsius * 9 / 5.0 + 32;
+        double kelvin = celsius + 273.15;
+
+        System.out.printf("%.2f(Celsius) är %.2f(Fahrenheit)%n", celsius, fahrenheit);
+        System.out.printf("%.2f(Celsius) är %.2f(kelvin)%n", celsius, kelvin);
+        scanner.close();
+
+         */
+        /*
+        //Räknare
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Värde A: ");
+        int a = scanner.nextInt();
+        System.out.println("Värde B:");
+        int b = scanner.nextInt();
+
+        int sum = a + b;
+
+        System.out.printf("%d + %d är lika med: %d%n", a, b, sum);
+        scanner.close();
+
+         */
+
+        //BMI-räknare
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ditt namn: ");
+        String name = scanner.nextLine();
+        System.out.println("din längd: ");
+        int length = scanner.nextInt();
+        System.out.println("din vikt: ");
+        int weight = scanner.nextInt();
+        double heightInMeters = length/100.0;
+        double bmi = weight / (heightInMeters * heightInMeters);
+
+        System.out.printf("Hej %s!%nDin längd är %d cm och din vikt är %d kg, du har BMI %.2f%n",
+                name, length, weight, bmi);
+
+
     }
 }
