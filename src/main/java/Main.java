9f544c05c4d242781e441ab35a751480a3d6dd6c
michaelinma <michael.inma@gmail.com>
2026-08-26 15:38:36 +0700

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,13 +8,24 @@ public class Main {
         System.out.println("Dagens datum är 2026-0");
 
          */
+        /*
         Scanner scanner = new Scanner(System.in);
         System.out.println("Pris på varan: ");
         double productPrice = scanner.nextDouble();
-        int percentageDiscount = 33;
+        int percentageDiscount = 27;
         double discount = productPrice * percentageDiscount / 100.0;
         double finalPrice = productPrice - discount;
         System.out.println("Original pris: " + productPrice + " kr" + ", pris efter rabatt: " + finalPrice + " kr");
         scanner.close();
+
+         */
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hej vad heter du");
+        String name = scanner.nextLine();
+        System.out.println("Hur gammal är du: ");
+        int age = scanner.nextInt();
+
+        System.out.println("Hej " + name + ", du är: " + age + " gammal.");
     }
 }
