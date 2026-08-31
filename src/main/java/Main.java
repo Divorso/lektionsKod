838deb36eee74cc79e8e3bca31ed393b12cc4c5f
michaelinma <michael.inma@gmail.com>
2026-08-31 16:45:39 +0700

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -57,7 +57,7 @@ public class Main {
         scanner.close();
 
          */
-
+        /*
         //BMI-räknare
         Scanner scanner = new Scanner(System.in);
         System.out.println("ditt namn: ");
@@ -68,9 +68,86 @@ public class Main {
         int weight = scanner.nextInt();
         double heightInMeters = length/100.0;
         double bmi = weight / (heightInMeters * heightInMeters);
+        if (bmi >= 18.5 && bmi < 25){
+            System.out.println("**standard vikt**");
 
+        }else if(bmi > 25){
+            System.out.println("**över vikt**");
+        } else if (bmi < 18.5) {
+            System.out.println("**under vikt**");
+        };
+        double inMeter = length *0.01;
+        System.out.println("din längd i meter är " +inMeter + " meter" );
         System.out.printf("Hej %s!%nDin längd är %d cm och din vikt är %d kg, du har BMI %.2f%n",
                 name, length, weight, bmi);
+                
+         */
+        /*
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Skriv 1-7");
+        int tal = scanner.nextInt();
+        String veckodag = "";
+        switch (tal) {
+            case 1 -> {
+                veckodag = "måndag";
+            }
+            case 2 -> {
+                veckodag = "tisdag";
+            }
+            case 3 -> {
+                veckodag = "onsdag";
+            }
+            case 4 -> {
+                veckodag = "torsdag";
+            }
+            case 5 -> {
+                veckodag = "fredag";
+            }
+            case 6 -> {
+                veckodag = "lördag";
+            }
+            case 7 -> {
+                veckodag = "söndag";
+            }
+        }
+        System.out.println(veckodag);
+        scanner.close();
+
+         */
+        /*
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("time to take off: ");
+        int tal = scanner.nextInt();
+
+        for (int i = tal; i >= 0; i--){
+            System.out.println(i);
+            if(i == 0){
+                System.out.println("take off!");
+            };
+        };
+         */
+
+        //Fizzbuzz
+        /*
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Skriv ett tal:");
+        int tal = scanner.nextInt();
+
+        if (tal % 3 == 0 && tal % 5 == 0) {
+            System.out.println("fizzbuzz");
+        } else if (tal % 5 == 0) {
+            System.out.println("fizz");
+        } else if (tal % 3 == 0) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println("neither");
+        }
+
+        scanner.close();
+
+         */
+
 
 
     }
